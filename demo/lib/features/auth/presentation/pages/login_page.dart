910c4513b64ef66c54aa4001db0d0686d69266b7
jmayta1984 <jmayta@pucp.edu.pe>
2025-06-10 11:04:18 -0500

    feat: update LoginPage

--- demo/lib/features/auth/presentation/pages/login_page.dart
+++ demo/lib/features/auth/presentation/pages/login_page.dart
@@ -52,82 +52,95 @@ class _LoginPageState extends State<LoginPage> {
               padding: const EdgeInsets.all(8.0),
               child: Stack(
                 children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      TextField(
-                        cursorColor: ColorPalette.primaryColor,
-                        controller: _usernameController,
-                        decoration: InputDecoration(
-                          suffixIcon: Icon(Icons.person),
-                          hintText: "Username",
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8),
-                            borderSide: BorderSide(
-                              width: 2,
-                              color: ColorPalette.primaryColor,
-                            ),
-                          ),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 8),
-                      TextField(
-                        cursorColor: ColorPalette.primaryColor,
-                        controller: _passwordController,
-                        decoration: InputDecoration(
-                          suffixIcon: IconButton(
-                            onPressed: () {
-                              setState(() {
-                                _isVisible = !_isVisible;
-                              });
-                            },
-                            icon: Icon(
-                              _isVisible
-                                  ? Icons.visibility
-                                  : Icons.visibility_off,
-                            ),
-                          ),
-                          hintText: "Password",
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8),
-                            borderSide: BorderSide(
-                              width: 2,
-                              color: ColorPalette.primaryColor,
-                            ),
-                          ),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                        ),
-                        obscureText: !_isVisible,
-                      ),
-                      SizedBox(height: 8),
-                      SizedBox(
-                        width: double.infinity,
-                        child: FilledButton(
-                          style: FilledButton.styleFrom(
-                            backgroundColor: ColorPalette.primaryColor,
-                          ),
-                          onPressed: () {
-                            context.read<AuthBloc>().add(
-                              LoginEvent(
-                                username: _usernameController.text,
-                                password: _passwordController.text,
+                  AbsorbPointer(
+                    absorbing: isLoading,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextField(
+                          cursorColor: ColorPalette.primaryColor,
+                          controller: _usernameController,
+                          decoration: InputDecoration(
+                            suffixIcon: Icon(Icons.person),
+                            hintText: "Username",
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                              borderSide: BorderSide(
+                                width: 2,
+                                color: ColorPalette.primaryColor,
                               ),
-                            );
-                          },
-                          child: Text("Sign in"),
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
                         ),
-                      ),
-                    ],
+                        SizedBox(height: 8),
+                        TextField(
+                          cursorColor: ColorPalette.primaryColor,
+                          controller: _passwordController,
+                          decoration: InputDecoration(
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  _isVisible = !_isVisible;
+                                });
+                              },
+                              icon: Icon(
+                                _isVisible
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                              ),
+                            ),
+                            hintText: "Password",
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                              borderSide: BorderSide(
+                                width: 2,
+                                color: ColorPalette.primaryColor,
+                              ),
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                          obscureText: !_isVisible,
+                        ),
+                        SizedBox(height: 8),
+                        SizedBox(
+                          width: double.infinity,
+                          child: FilledButton(
+                            style: FilledButton.styleFrom(
+                              backgroundColor: ColorPalette.primaryColor,
+                            ),
+                            onPressed: () {
+                              context.read<AuthBloc>().add(
+                                LoginEvent(
+                                  username: _usernameController.text,
+                                  password: _passwordController.text,
+                                ),
+                              );
+                            },
+                            child: Text("Sign in"),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                   if (isLoading)
                     Center(
-                      child: CircularProgressIndicator(
-                        color: ColorPalette.primaryColor,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade300,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        width: 90,
+                        height: 90,
+                        child: Center(
+                          child: CircularProgressIndicator(
+                            color: ColorPalette.primaryColor,
+                          ),
+                        ),
                       ),
                     ),
                 ],
